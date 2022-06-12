56b774c223613e8d4e96a7cd77ae114e7763a610
ThePyLord <geraldfeu@gmail.com>
2022-06-12 13:47:53 -0500

    revert to original style

--- src/renderer.ts
+++ src/renderer.ts
@@ -3,22 +3,20 @@ import './styles/game.css'
 console.log('👋 This message is being logged by "renderer.js", included via webpack')
 // import { Game } from './components/Game'
 import './app/main'
-import './App'
+// import './app/App'
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // No Node.js APIs are available in this process unless
 // nodeIntegration is set to true in webPreferences.
 // Use preload.js to selectively enable features
 // needed in the renderer process.
-// window.api.createWin()
-
 
 const choices = document.querySelector('.choice').children as HTMLCollectionOf<HTMLElement>
 for (let i = 0; i < choices.length; i++) {
 	const element = choices[i];
-	element.addEventListener('click', function()  {
+	element.addEventListener('click', function () {
 		this.classList.toggle('clicked')
-		if(this.classList.contains('clicked')) {
+		if (this.classList.contains('clicked')) {
 			this.style.color = 'rgb(84, 118, 192)'
 		}
 		else {
@@ -29,12 +27,12 @@ for (let i = 0; i < choices.length; i++) {
 const btn = document.querySelector('.btn') as HTMLElement
 window.onload = () => {
 	const appTheme = localStorage.getItem('theme')
-	if(appTheme === 'dark') {
+	if (appTheme === 'dark') {
 		document.body.classList.add('dark-mode')
 	}
-	else if(appTheme === 'light') {
+	else if (appTheme === 'light') {
 		document.body.classList.remove('dark-mode')
-		btn.classList.remove('active') 
+		btn.classList.remove('active')
 		btn.style.color = '#000'
 	}
 }
@@ -45,7 +43,7 @@ btn.addEventListener('click', toggleTheme)
 function toggleTheme() {
 	document.body.classList.toggle('dark-mode')
 	this.classList.toggle('active')
-	if(this.classList.contains('active')) {
+	if (this.classList.contains('active')) {
 		this.style.color = '#fff'
 		localStorage.setItem('theme', 'dark')
 	}
