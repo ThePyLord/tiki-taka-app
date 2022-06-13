bf3c16dfedaf35ae4a054675b0e8c66e4129d5ec
ThePyLord <geraldfeu@gmail.com>
2022-06-13 08:58:22 -0500

    no change

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import '../styles/homepage.css'
+
+export default function HomePage() {
+	useEffect(() => {
+		console.log('loaded homepage.')
+	}, [])
+	return (
+		<div className='container'>
+			<h1 className='title'>Home Page</h1>
+			<Link to="/welcome">Welcome Page</Link>
+		</div>
+	)
+}
